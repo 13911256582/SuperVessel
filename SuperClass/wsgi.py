a25cbadae562fdfcf592056f70bf04c76c2e1775
13911256582@163.com <13911256582@163.com>
2015-02-01 11:32:24 +0800

    change wsgi.py for apache deploy

--- SuperClass/wsgi.py
+++ SuperClass/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/home/shaol/Documents/projects/SuperVessel/SuperClass')
+sys.path.append('/home/shaol/Documents/projects/SuperVessel')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SuperClass.settings")
 
 from django.core.wsgi import get_wsgi_application
